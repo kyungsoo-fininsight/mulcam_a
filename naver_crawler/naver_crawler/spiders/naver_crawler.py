022d7174d59d7b1c32ddec86d83abcf922abec55
kyungsoo-fininsight <kshong@finisight.co.kr>
2020-08-28 09:46:56 +0900

    2 depth request

--- naver_crawler/naver_crawler/spiders/naver_crawler.py
+++ naver_crawler/naver_crawler/spiders/naver_crawler.py
@@ -7,10 +7,14 @@ class NaverSpider(scrapy.Spider):
 
     def start_requests(self):
         urls = [
-            "https://news.naver.com/main/read.nhn?mode=LSD&mid=shm&sid1=102&oid=001&aid=0011729190"
+            "https://search.naver.com/search.naver?query=%EA%B8%88%EB%A6%AC&where=news&ie=utf8&sm=nws_hty"
         ]
         for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+            yield scrapy.Request(url=url, callback=self.parse_list)
+
+    def parse_list(self, response):
+        url = "https://news.naver.com/main/read.nhn?mode=LSD&mid=sec&sid1=101&oid=001&aid=0011841162"
+        yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
         item = NaverCrawlerItem()
